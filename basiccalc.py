00270e0a9be728a74e0f75c8e83558de58fcb8bb
Bharath Nandan <bharath13431@gmail.com>
2026-06-30 18:55:50 +1000

    Modified calculator

--- basiccalc.py
+++ basiccalc.py
@@ -18,21 +18,24 @@ choice = ""
 while choice != "5":
     choice = input("Choose: ") 
     if choice == "1":
-        a = int(input("First number: "))
-        b = int(input("Second number: "))
+        a = float(input("First number: "))
+        b = float(input("Second number: "))
         print("Result = ", add(a, b))
     elif choice == "2":
-        a = int(input("First number: "))
-        b = int(input("Second number: "))
+        a = float(input("First number: "))
+        b = float(input("Second number: "))
         print("Result = ",subtract(a, b))
     elif choice == "3":
-        a = int(input("First number: "))
-        b = int(input("Second number: "))
+        a = float(input("First number: "))
+        b = float(input("Second number: "))
         print("Result = ",multiply(a, b))
     elif choice == "4":
-        a = int(input("First number: "))
-        b = int(input("Second number: "))
-        print("Result = ",divide(a, b))
+        a = float(input("First number: "))
+        b = float(input("Second number: "))
+        if b == 0:
+            print("Cannot divide by zero")
+        else:
+            print("Result = ",divide(a, b))
     elif choice == "5":
         pass
     else:
